9270c50afd7a0829ce5b04b51d560c402d3e6d6e
Ruishu Cao <65318759+rrarissa@users.noreply.github.com>
2022-06-03 23:55:02 -0400

    Create data.js

---
+++ Job Review Project/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'susan smith',
+    job: 'web developer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    text:
+"I've worked in this company for around 7 years now and I have enjoyed my time here so far. Not only are the staff very nice, but the job also pays well too. One thing that really helped me in this company was the food and snack program. When I graduated from college and just got into the company I was struggling with money. I was unable to afford a plate to eat but after getting into the company I was able to eat due to the free lunches and snacks provided, and keep myself energized which enhanced my working ability. This was really helpful as I was able to get myself back on my feet. Going into my 8th year I now have a house and am able to support myself."
+  },
+  {
+    id: 2,
+    name: 'anna johnson',
+    job: 'web designer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+    text:
+      'If given the opportunity to work your own way, web development can be very challenging with different web browsers that needs supporting along with new technologies like html5 coming out, how to use as much of it as you can while making it all work on every browser smoothly.',
+  },
+  {
+    id: 3,
+    name: 'peter jones',
+    job: 'intern',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    text:
+      'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+  },
+  {
+    id: 4,
+    name: 'bill anderson',
+    job: 'the boss',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    text:
+      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+]
+
+export default reviews

86beaccaedc817aaca8e5e917b32247765b5c0ac
Ruishu Cao <65318759+rrarissa@users.noreply.github.com>
2022-06-04 12:34:27 -0400

    Create data.js

---
+++ Question & answer/data.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    id: 1,
+    title: 'Q. What if I incur overdraft or other fees as a result of the delay?',
+    info:
+      'A. We will not charge you any fees on your TD Bank account(s) as a result of these delays and we will reimburse you for any fees you may incur from other banks as a result of these delays. Our employees will work with you individually to resolve these outstanding issues. Please drop by one of our stores, which are open early and late each day. ',
+  },
+  {
+    id: 2,
+    title: 'How do I change my My Page password?',
+    info:
+      'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
+  },
+  {
+    id: 3,
+    title: 'What is BankID?',
+    info:
+      'Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.',
+  },
+  {
+    id: 4,
+    title: 'Whose birth number can I use?',
+    info:
+      'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.',
+  },
+  {
+    id: 5,
+    title: 'When do I recieve a password ordered by letter?',
+    info:
+      'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
+  },
+]
+export default questions
